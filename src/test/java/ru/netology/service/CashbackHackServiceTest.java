d398ccac5bb19ae0e46e877a6b7084b3c3e8cb99
Kira <kira_pavlenko_13@mail.ru>
2023-02-01 20:30:08 +0300

    Junit4-platform added

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -9,11 +9,23 @@ public class CashbackHackServiceTest {
     @Test
     public void remain() {
         CashbackHackService service = new CashbackHackService();
-        assertEquals(1, service.remain(999);
+        assertEquals(1, service.remain(999));
     }
 
     @Test
     public void testEqual() {
+        CashbackHackService service = new CashbackHackService();
+        assertEquals(0, service.remain(1000));
+    }
+
+    @org.junit.jupiter.api.Test
+    void testRemain() {
+        CashbackHackService service = new CashbackHackService();
+        assertEquals(1, service.remain(999));
+    }
+
+    @org.junit.jupiter.api.Test
+    void Equal() {
         CashbackHackService service = new CashbackHackService();
         assertEquals(0, service.remain(1000));
     }
